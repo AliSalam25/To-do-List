0b7124f532127bbe93182e1763618cf6bba20da5
Ali <Ali.Salam@city.ac.uk>
2025-12-30 17:55:57 +0000

    Basic structure made

--- todo.py
+++ todo.py
@@ -1,1 +1,16 @@
-print("Test commit")
+def show_tasks(tasks):
+    print("\nYour tasks:")
+    for task in tasks:
+        print(task)
+
+
+def add_task(tasks):
+    pass
+
+
+def mark_done(tasks):
+    pass
+
+
+def delete_task(tasks):
+    pass
